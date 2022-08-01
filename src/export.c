815cb6a63860168174e4e31d69374a4b03839e4f
axel lukongo <axellukongo@MacBook-Air-de-axel.local>
2022-08-01 22:14:40 +0200

    cm

--- src/export.c
+++ src/export.c
@@ -6,24 +6,26 @@
 /*   By: alukongo <alukongo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 19:58:40 by alukongo          #+#    #+#             */
-/*   Updated: 2022/07/30 00:52:37 by alukongo         ###   ########.fr       */
+/*   Updated: 2022/08/01 21:59:31 by alukongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"../include/minishell.h"
 
-void	sort_list(t_list **env, int (*cmp)())
+void	sort_list(t_list **env)
 {
 	t_list	*current;
 	t_list	*next;
 	t_list	*next_next;
 	int size_list;
 
+	(void) next;
+	(void) next_next;
+	(void) current;
 	current = *env;
 	size_list = ft_lstsize(current);
 	while (size_list)
 	{
-		if()
 		size_list--;
 	}
 }
