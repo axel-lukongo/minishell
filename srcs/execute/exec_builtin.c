aeda80477ac04639d1a5224e3b6030758e98ace7
axel lukongo <axellukongo@MacBook-Air-de-axel.local>
2022-08-11 17:10:06 +0200

    i remplace by getcwd

--- srcs/execute/exec_builtin.c
+++ srcs/execute/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: alukongo <alukongo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 12:50:59 by denissereno       #+#    #+#             */
-/*   Updated: 2022/08/11 16:18:32 by alukongo         ###   ########.fr       */
+/*   Updated: 2022/08/11 17:09:09 by alukongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -170,6 +170,14 @@ void	my_unset(t_global *g, char **cmd)
 	g->last_return = 0;
 }
 
+void my_cd2(t_global *g)
+{
+	change_value_by_name(g, "OLDPWD", get_node_by_name(g->env, "PWD")->value);
+	chdir(get_node_by_name(g->env, "HOME")->value);
+	push_ustack(g->dir_stack, get_node_by_name(g->env, "HOME")->value);
+	change_value_by_name(g, "PWD", getcwd((char *)NULL, 0));
+}
+
 void	cd_dash_or_nothing(t_global *g, char **cmd)
 {
 	char	*value;
@@ -183,7 +191,7 @@ void	cd_dash_or_nothing(t_global *g, char **cmd)
 		{
 			chdir(value);
 			push_ustack(g->dir_stack, value);
-			change_value_by_name(g, "PWD", value);
+			change_value_by_name(g, "PWD", getcwd((char *)NULL, 0));
 		}
 		return ;
 	}
@@ -192,178 +200,66 @@ void	cd_dash_or_nothing(t_global *g, char **cmd)
 		change_value_by_name(g, "OLDPWD", get_node_by_name(g->env, "PWD")->value);
 		chdir(get_node_by_name(g->env, "HOME")->value);
 		push_ustack(g->dir_stack, get_node_by_name(g->env, "HOME")->value);
-		change_value_by_name(g, "PWD", get_node_by_name(g->env, "HOME")->value);
-		// CODER ICI DEPLACER JUSQUAU HOME : CD --
+		change_value_by_name(g, "PWD", getcwd((char *)NULL, 0));
+		//CODER ICI DEPLACER JUSQUAU HOME : CD --
 	}
 }
 
+void my_cd(t_global *g, char **cmd)
+{
+	int	cd;
+	if	(cmd[1] && !dir_change_stack(cmd[1]))
+	{
+		change_value_by_name(g, "OLDPWD", get_node_by_name(g->env, "PWD")->value);
+		if	(cmd[1][0] != '/')
+			cd = chdir(ft_strjoin(get_node_by_name(g->env, "PWD")->value, ft_strjoin("/", cmd[1], &g->alloc), &g->alloc)) ;
+		else
+			cd = chdir(cmd[1]);
+		if (!cd)
+		{
+			if (!ft_strcmp(cmd[1], ".."))
+			{
+				push_ustack(g->dir_stack, del_last_path(g, get_node_by_name(g->env, "PWD")->value));
+				change_value_by_name(g, "PWD", getcwd((char *)NULL, 0));
+			}
+			else
+			{
+				push_ustack(g->dir_stack, ft_strjoin(get_node_by_name(g->env, "PWD")->value, ft_strjoin("/", cmd[1], &g->alloc), &g->alloc));
+				change_value_by_name(g, "PWD", getcwd((char *)NULL, 0));
+			}
+			g->last_return = 0;
+		}
+		else
+			cd_error_msg(g, cmd);
+	}
+}
 
 void	execute_builtin(t_global *g, char **cmd)
 {
-	//char	*value;
-	int		cd;
-
 	if (!ft_strcmp(cmd[0], "exit"))
-	{
 		my_exit(g, cmd);
-		// if (cmd[1])
-		// { // A FINIR AVEC LA LIMIT 64 
-		// 	if (ft_strisdigit(cmd[1]) && ft_atoi_u64(cmd[1]) < 9223372036854775807)
-		// 	{
-		// 		if (cmd[2] != NULL)
-		// 		{
-		// 			printf("bash: exit: too many arguments\n");
-		// 			g->last_return = 1;
-		// 		}
-		// 		else
-		// 			exit(ft_atoi(cmd[1]) % 256);
-		// 	}
-		// 	else
-		// 	{
-		// 		printf("bash: exit: exit: numeric argument required\n");
-		// 		exit(255);
-		// 	}
-		// }
-		// else
-		// 	exit(0);
-	}
 	else if	(!ft_strcmp(cmd[0], "env"))
-	{
 		my_env(g, cmd);
-		// if (!cmd[1])
-		// {
-		// 	print_list_env(g->env);
-		// 	g->last_return = 0;
-		// }
-		// else
-		// {
-		// 	value = get_value_by_name(g->env, cmd[1]);
-		// 	if (!value)
-		// 	{
-		// 		write(2, "env: ", 5);
-		// 		write(2, cmd[1], ft_strlen(cmd[1]));
-		// 		write(2, ": No such file or directory\n", 28);
-		// 		g->last_return = 127;
-		// 		return ;
-		// 	}
-		// 	g->last_return = 0;
-		// }
-	}
 	else if	(!ft_strcmp(cmd[0], "unset"))
-	{
 		my_unset(g, cmd);
-	// 	if (!cmd[1])
-	// 	{
-	// 		write(2, "unset: not enough arguments\n", 28);
-	// 		return ;
-	// 	}
-	// 	destroy_env_var(g, cmd[1]);
-	// 	g->last_return = 0;
-	}
 	else if	(!ft_strcmp(cmd[0], "cd")) // CHANGER PWD PAR GETCWD
 	{
 		if (!get_node_by_name(g->env, "PWD"))
 			return ;
 		if (!cmd[1] && get_node_by_name(g->env, "HOME"))
-		{
-			//cd_dash_or_nothing(g, cmd);
-			change_value_by_name(g, "OLDPWD", get_node_by_name(g->env, "PWD")->value);
-			chdir(get_node_by_name(g->env, "HOME")->value);
-			push_ustack(g->dir_stack, get_node_by_name(g->env, "HOME")->value);
-			change_value_by_name(g, "PWD", get_node_by_name(g->env, "HOME")->value);
-		}
+			my_cd2(g);
 		else if	(cmd[1] && !dir_change_stack(cmd[1]))
-		{
-			//printf("%s\n", cmd[1]);
-			change_value_by_name(g, "OLDPWD", get_node_by_name(g->env, "PWD")->value);
-			if	(cmd[1][0] != '/')
-				cd = chdir(ft_strjoin(get_node_by_name(g->env, "PWD")->value, ft_strjoin("/", cmd[1], &g->alloc), &g->alloc)) ;
-			else
-				cd = chdir(cmd[1]);
-			if (!cd)
-			{
-				if (!ft_strcmp(cmd[1], ".."))
-				{
-					push_ustack(g->dir_stack, del_last_path(g, get_node_by_name(g->env, "PWD")->value));
-					change_value_by_name(g, "PWD", ft_strjoin(get_node_by_name(g->env, "PWD")->value, ft_strjoin("/", cmd[1], &g->alloc), &g->alloc));
-				}
-				else
-				{
-					push_ustack(g->dir_stack, ft_strjoin(get_node_by_name(g->env, "PWD")->value, ft_strjoin("/", cmd[1], &g->alloc), &g->alloc));
-					change_value_by_name(g, "PWD", ft_strjoin(get_node_by_name(g->env, "PWD")->value, ft_strjoin("/", cmd[1], &g->alloc), &g->alloc));
-				}
-				g->last_return = 0;
-			}				
-			else
-			{
-				cd_error_msg(g, cmd);
-				// write(2, "cd: no such file or directory: ", 31);
-				// write(2, cmd[1], ft_strlen(cmd[1]));
-				// write(2, "\n", 1);
-				// g->last_return = 1;
-			}
-		}
-		/*
-		else if (cmd[1] && !dir_change_stack(cmd[1]) && cmd[1][0] == '/')
-		{
-			change_value_by_name(g, "OLDPWD", get_node_by_name(g->env, "PWD")->value);
-			cd = chdir(cmd[1]);
-			if (!cd)
-			{
-				push_ustack(g->dir_stack, ft_strjoin(get_node_by_name(g->env, "HOME")->value, ft_strjoin("/", cmd[1], &g->alloc), &g->alloc));
-				change_value_by_name(g, "PWD", cmd[1]);
-				g->last_return = 0;
-			}
-			else
-			{
-				write(2, "cd: no such file or directory: ", 31);
-				write(2, cmd[1], ft_strlen(cmd[1]));
-				write(2, "\n", 1);
-				g->last_return = 1;
-			}
-		}*/
+			my_cd(g, cmd);
 		else if (dir_change_stack(cmd[1]))
 		{
 			cd_dash_or_nothing(g, cmd);
-			// if (cmd[1][0] == '-' && ft_strlen(cmd[1]) == 1)
-			// {
-			// 	value = get_value_ustack(g->dir_stack, 1);
-			// 	if (!value)
-			// 		write(2, "cd: no such entry in dir stack\n", 31);
-			// 	else
-			// 	{
-			// 		chdir(value);
-			// 		push_ustack(g->dir_stack, value);
-			// 		change_value_by_name(g, "PWD", value);
-			// 	}
-			// 	return ;
-			// }
-			// else if (cmd[1][0] == '-' && ft_strlen(cmd[1]) == 2 && cmd[1][1] == '-')
-			// {
-			// 	change_value_by_name(g, "OLDPWD", get_node_by_name(g->env, "PWD")->value);
-			// 	chdir(get_node_by_name(g->env, "HOME")->value);
-			// 	push_ustack(g->dir_stack, get_node_by_name(g->env, "HOME")->value);
-			// 	change_value_by_name(g, "PWD", get_node_by_name(g->env, "HOME")->value);
-			// 	// CODER ICI DEPLACER JUSQUAU HOME : CD --
-			// }
-			// printf("---------je rentre ici 1----------\n");
-			// cd = ft_atoi(ft_split(cmd[1], '-', g->alloc)[0]);
-			// value = get_value_ustack(g->dir_stack, cd);
-			// if (!value)
-			// 	write(2, "cd: no such entry in dir stack\n", 31);
-			// else
-			// {
-			// 	chdir(value);
-			// 	push_ustack(g->dir_stack, value);
-			// 	change_value_by_name(g, "PWD", value);
-			// }
 		}
 	}
-	else if (!ft_strcmp(cmd[0], "export"))
+	else if	(!ft_strcmp(cmd[0], "export"))
 	{
 		char	**env;
 		t_env	*node;
-		if (!cmd[1])
+		if	(!cmd[1])
 		{
 			//printf("je rentre ici");
 			my_export(g->env);
